c03c407ecfc3cdd6216c491dbec155faca6c7109
Nguyen Thien An <ngthienaans@gmail.com>
2024-05-26 20:09:53 +0700

    fix: racist

--- Gameplay.py
+++ Gameplay.py
@@ -635,7 +635,7 @@ class Gameplay:
         screen_size = ((self.maze_row + self.minimap_grid_size[0] + 1) * self.bg_cell_size,
                        (self.maze_col + self.minimap_grid_size[1] + 1) * self.bg_cell_size)
 
-        self.bg_surface = morph_image(RESOURCE_PATH + "nigga.png", screen_size)
+        self.bg_surface = morph_image(RESOURCE_PATH + "gameplay_bg.png", screen_size)
 
         maze_surface = pygame.Surface(((self.maze_row) * (self.bg_cell_size), ((self.maze_col) * self.bg_cell_size)),
                                       pygame.SRCALPHA, 32).convert_alpha()
